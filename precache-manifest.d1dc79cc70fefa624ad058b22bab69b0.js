04331f6dc54466d0c7f67575afb56a7974ab93f4
Bartosz Marciniec <marciniec.bartosz@gmail.com>
2020-01-21 20:36:55 +0100

    Updates

---
+++ precache-manifest.d1dc79cc70fefa624ad058b22bab69b0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68cb8a2d2160c0d7cda51438eb09ec6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1ca6f58a61f8c9b74bd",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e1ca6f58a61f8c9b74bd",
+    "url": "/static/js/2.5cba68a7.chunk.js"
+  },
+  {
+    "revision": "d9fe3a40a207cf62f3e97fc158129856",
+    "url": "/static/js/2.5cba68a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3dd30ab3c8642c0ff269",
+    "url": "/static/js/main.74b42d7e.chunk.js"
+  },
+  {
+    "revision": "a9918639d460deb7c433",
+    "url": "/static/js/runtime-main.b2f638c6.js"
+  },
+  {
+    "revision": "322b60565f48130d5bc6ea67ea51de28",
+    "url": "/static/media/machine.322b6056.png"
+  },
+  {
+    "revision": "9d58e0b19d45d7a811cc917c2c373238",
+    "url": "/static/media/slide1.9d58e0b1.png"
+  },
+  {
+    "revision": "ee9e0e90d84e4d19365d2974bd439ba1",
+    "url": "/static/media/slide2.ee9e0e90.png"
+  },
+  {
+    "revision": "c309c0ae461ef37378ed8caec2062f65",
+    "url": "/static/media/slide3.c309c0ae.png"
+  },
+  {
+    "revision": "3264c8718cd956f6009e80d48b7da3b6",
+    "url": "/static/media/tenby-five.3264c871.otf"
+  },
+  {
+    "revision": "b812192081ce06b5ea67eab218035d9f",
+    "url": "/static/media/worker.b8121920.png"
+  },
+  {
+    "revision": "e5a1c0959df27de188eb26fb8f6d55d0",
+    "url": "/static/media/worker.e5a1c095.png"
+  }
+]);
