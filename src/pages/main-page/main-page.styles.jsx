aba9f04279ab6f48752f23ba2b3ad8444c937801
Marcin Banik <mrn.banik@gmail.com>
2020-03-13 13:13:54 +0100

    Alter font size, weight

--- src/pages/main-page/main-page.styles.jsx
+++ src/pages/main-page/main-page.styles.jsx
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const StyledMainPageContainer = styled.section`
   h1 {
     font-size: 3.2rem;
-    font-family: 'Saira Condensed Semibold';
+    font-family: bold;
   }
 `;
