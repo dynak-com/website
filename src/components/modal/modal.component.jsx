152a405fff6e101b40963c249eba38595244e6ce
Marcin Banik <mrn.banik@gmail.com>
2020-03-19 23:05:12 +0100

    Work in progress

--- src/components/modal/modal.component.jsx
+++ src/components/modal/modal.component.jsx
@@ -17,7 +17,10 @@ const Modal = React.forwardRef((props, ref) => {
       aria-labelledby={modalId}
       aria-hidden='true'
     >
-      <StyledModalDialog className='text-center' role='document'>
+      <StyledModalDialog
+        className='modal-dialog-centered text-center'
+        role='document'
+      >
         <div className='modal-content'>
           <div className='modal-header'>
             <h5 className='modal-title' id={`${modalId}Label`}>
