f02f8a95d8cc1b3cf45c1e31dac7636629d4e0c6
Marcin Banik <mrn.banik@gmail.com>
2020-03-20 18:43:50 +0100

    Try to fix modal issue

--- src/components/modal/modal.component.jsx
+++ src/components/modal/modal.component.jsx
@@ -17,36 +17,31 @@ const Modal = React.forwardRef((props, ref) => {
       aria-labelledby={modalId}
       aria-hidden='true'
     >
-      <div>
-        <StyledModalDialog
-          className='modal-dialog modal-dialog-centered'
-          role='document'
-        >
-          <div className='modal-content text-center'>
-            <div className='modal-header'>
-              <h5 className='modal-title' id={`${modalId}Label`}>
-                {content.thanks}
-              </h5>
-              <button
-                type='button'
-                className='close'
-                data-dismiss='modal'
-                aria-label='Close'
-              >
-                <span aria-hidden='true'>&times;</span>
-              </button>
-            </div>
-            <div className='modal-body'>
-              <h3 className={headerTextAlignment}>{content.sent}</h3>
-            </div>
-            <div className='text-right p-2'>
-              <button type='button' className='btn btn-sm' data-dismiss='modal'>
-                {content.close}
-              </button>
-            </div>
+      <StyledModalDialog className='modal-dialog text-center' role='document'>
+        <div className='modal-content'>
+          <div className='modal-header'>
+            <h5 className='modal-title' id={`${modalId}Label`}>
+              {content.thanks}
+            </h5>
+            <button
+              type='button'
+              className='close'
+              data-dismiss='modal'
+              aria-label='Close'
+            >
+              <span aria-hidden='true'>&times;</span>
+            </button>
           </div>
-        </StyledModalDialog>
-      </div>
+          <div className='modal-body'>
+            <h3 className={headerTextAlignment}>{content.sent}</h3>
+          </div>
+          <div className='text-right p-2'>
+            <button type='button' className='btn btn-sm' data-dismiss='modal'>
+              {content.close}
+            </button>
+          </div>
+        </div>
+      </StyledModalDialog>
     </StyledModalContainer>
   );
 });
