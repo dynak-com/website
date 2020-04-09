cddb4b28bb8725304f15d54a0319ae9bc8266f13
Marcin Banik <mrn.banik@gmail.com>
2020-04-09 20:51:54 +0200

    Fix development environment issue

--- src/App.js
+++ src/App.js
@@ -16,44 +16,37 @@ const contentData = JSON.parse(JSON.stringify(content));
 
 const App = () => {
   const { lang } = useComponent();
-  const content = contentData.find(item => item.lang === lang);
-  const onRedirectToHealthChecker = () =>
-    (window.location.href = 'https://www.dynak.com:8080/health');
+  const content = contentData.find((item) => item.lang === lang);
   return (
-    <Switch>
-      <Route exact path='/health' render={onRedirectToHealthChecker()} />
-      <Route patch='*'>
-        <StyledAppContainer>
-          <GlobalStyles />
-          <Navbar content={content.nav} targetId='headerNavbarMenu' />
-          <Switch>
-            <Route
-              exact
-              path='/'
-              component={() => <MainPage content={content.home} />}
-            />
-            <Route
-              exact
-              path='/about'
-              component={() => <AboutPage content={content.about} />}
-            />
-            <Route
-              exact
-              path='/projects'
-              component={() => <ProjectsPage content={content.projects} />}
-            />
-            <Route
-              exact
-              path='/contact'
-              component={() => <ContactPage content={content.contact} />}
-            />
-            <Route component={() => <ErrorPage content={content.error} />} />
-          </Switch>
-          <Footer />
-          <ModalsContainer modalsData={content.modal} />
-        </StyledAppContainer>
-      </Route>
-    </Switch>
+    <StyledAppContainer>
+      <GlobalStyles />
+      <Navbar content={content.nav} targetId='headerNavbarMenu' />
+      <Switch>
+        <Route
+          exact
+          path='/'
+          component={() => <MainPage content={content.home} />}
+        />
+        <Route
+          exact
+          path='/about'
+          component={() => <AboutPage content={content.about} />}
+        />
+        <Route
+          exact
+          path='/projects'
+          component={() => <ProjectsPage content={content.projects} />}
+        />
+        <Route
+          exact
+          path='/contact'
+          component={() => <ContactPage content={content.contact} />}
+        />
+        <Route component={() => <ErrorPage content={content.error} />} />
+      </Switch>
+      <Footer />
+      <ModalsContainer modalsData={content.modal} />
+    </StyledAppContainer>
   );
 };
 
