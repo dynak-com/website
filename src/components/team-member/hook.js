70495640785877c7df7675f196737e97118f0572
CristinaVolk <kristinavolk4@tut.by>
2020-05-05 15:19:33 +0200

    correcting logo margin-right, abortController [added]

--- src/components/team-member/hook.js
+++ src/components/team-member/hook.js
@@ -1,22 +1,38 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import AbortController from 'abort-controller'
 
-const useComponent = (username) => {
-  const [avatarURL, setAvatarUrl] = useState('');
-  useEffect(() => {
-    fetchGitlabAvatar(username, setAvatarUrl);
-  }, [username, setAvatarUrl]);
+const abortController = new AbortController();
+
+const useComponent = ( username ) =>
+{
+  const [ avatarURL, setAvatarUrl ] = useState( '' );
+  useEffect( () =>
+  {
+    fetchGitlabAvatar( username, setAvatarUrl );
+    return () =>
+    {
+      abortController.abort();
+    };
+  }, [ username, setAvatarUrl ] );
   return {
     avatarURL,
   };
 };
 
-async function fetchGitlabAvatar(username, setAvatarUrl) {
-  let result = await axios.get(
-    `https://gitlab.com/api/v4/users?username=${username}&size=180`
-  );
-  let { avatar_url } = await result.data[0];
-  setAvatarUrl(avatar_url);
-}
+function fetchGitlabAvatar ( username, setAvatarUrl )
+{
+  const url = `https://gitlab.com/api/v4/users?username=${ username }&size=180`;
+  try
+  {
+    fetch( url, { method: 'get', signal: abortController.signal } )
+      .then( response => response.json() )
+      .then( data => setAvatarUrl( data[ 0 ].avatar_url ) );
+  } catch ( error )
+  {
+    if ( !abortController.signal.aborted )
+      console.error(error);
+  }
+};
 
 export default useComponent;
+
