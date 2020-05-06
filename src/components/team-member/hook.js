4e7db4292576529557308497e61b3b817ade5bcd
CristinaVolk <kristinavolk4@tut.by>
2020-05-06 11:44:28 +0000

    Resolve "[Front-end] Add cleanup to the hook in the TeamMember component."

--- src/components/team-member/hook.js
+++ src/components/team-member/hook.js
@@ -1,27 +1,29 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import AbortController from 'abort-controller';
 
-const useComponent = ( username ) =>
-{
-  const [ avatarURL, setAvatarUrl ] = useState( '' );
-  useEffect( () =>
-  {
-    fetchGitlabAvatar( username, setAvatarUrl );
-  }, [ username, setAvatarUrl ] );
-  return {
-    avatarURL,
-  };
+const useComponent = (username) => {
+    const [avatarURL, setAvatarUrl] = useState('');
+    useEffect(() => {
+        const abortController = new AbortController();
+        fetchGitlabAvatar(username, setAvatarUrl, abortController.signal);
+        return () => {
+            abortController.abort();
+        };
+    }, [username, setAvatarUrl]);
+    return {
+        avatarURL,
+    };
 };
 
-async function fetchGitlabAvatar ( username, setAvatarUrl )
-{
-  let result = await axios.get(
-    `https://gitlab.com/api/v4/users?username=${ username }&size=180`
-  );
-  let { avatar_url } = await result.data[ 0 ];
-  setAvatarUrl( avatar_url );
+async function fetchGitlabAvatar(username, setAvatarUrl, controllerSignal) {
+    try {
+        const url = `https://gitlab.com/api/v4/users?username=${username}&size=180`;
+        let result = await fetch(url, { method: 'get', signal: controllerSignal }).then((response) => response.json());
+        const { avatar_url } = await result[0];
+        setAvatarUrl(avatar_url);
+    } catch (error) {
+        if (!controllerSignal.aborted) console.error(error);
+    }
 }
 
 export default useComponent;
-
-
