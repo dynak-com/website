1ba85cff9545f3780cf6f580e78810d06b78a6e2
CristinaVolk <kristinavolk4@tut.by>
2020-05-18 13:13:00 +0200

    testing the hook of setting the current year [implemented]

--- src/components/footer/footer.test.js
+++ src/components/footer/footer.test.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import Footer from './footer.component';
+import useComponent from './hook';
 
 describe('Footer component', () => {
     const component = shallow(<Footer />);
     const wrapper = mount(<Footer />);
-    //console.log(wrapper.debug());
 
     it('should render 2 spans', () => {
         const footer = component.find('footer');
@@ -18,15 +18,33 @@ describe('Footer component', () => {
         expect(rodoSign.text()).toEqual('RODO');
     });
 
-    it('should change the text color on hover', () => {
-        const rodoSign = wrapper.find('.rodo');
-        rodoSign.simulate('mouseover');
-        //toHaveStyle('color: #ddd');
-        expect(rodoSign.text()).toEqual('RODO');
-    });
-
     it('renders correctly', () => {
         const tree = renderer.create(<Footer />).toJSON();
         expect(tree).toMatchSnapshot();
+    });
+});
+
+function HookWrapper(props) {
+    const hook = props.hook ? props.hook() : undefined;
+    return <HookWrapper hook={hook} />;
+}
+
+describe('Footer hook', () => {
+    const component = shallow(<Footer />);
+    it('renders correctly', () => {
+        expect(component.exists()).toBeTruthy();
+    });
+
+    it('should set init value', () => {
+        const wrapper = shallow(<HookWrapper hook={() => useComponent()} />);
+
+        const hook = wrapper.props().hook;
+        const currentYear = new Date().getFullYear();
+
+        const { year, onPrivacyPolicyModalOpenDone } = hook;
+        expect(year).toEqual(currentYear);
+
+        const property = component.find('span').at(1).prop('onClick');
+        expect(typeof property === 'function' ? true : false).toBeTruthy();
     });
 });
