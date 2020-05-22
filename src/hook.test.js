c2d836d320d3026183f8cbf75a3a2941fda2552f
CristinaVolk <kristinavolk4@tut.by>
2020-05-22 19:21:06 +0200

    App test [updated]

---
+++ src/hook.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import useComponent from './hook';
+
+function HookWrapper(props) {
+    const hook = props.hook ? props.hook() : undefined;
+    return <HookWrapper hook={hook} />;
+}
+
+describe('', () => {
+    it('should set init language value', () => {
+        const wrapper = shallow(<HookWrapper hook={() => useComponent()} />);
+        const hook = wrapper.props().hook;
+        const { lang, onSetLang } = hook;
+        expect(lang).toEqual('pl');
+    });
+});
