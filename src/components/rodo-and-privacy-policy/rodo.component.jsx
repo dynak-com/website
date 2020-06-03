4f41d646c87c62cb27d0dfa7a0df9949593b4e9e
CristinaVolk <kristinavolk4@tut.by>
2020-06-03 12:30:13 +0200

    rodoComponent [translated]

--- src/components/rodo-and-privacy-policy/rodo.component.jsx
+++ src/components/rodo-and-privacy-policy/rodo.component.jsx
@@ -14,64 +14,39 @@ const RodoContent = ({ rodoContent }) => {
                 </a>
                 {t(rodoContent.par2)}
             </p>
-            <span>1. POSTANOWIENIA OGÓLNE</span>
+            <span>{t(rodoContent.generalProvisions)}</span>
             <p>
-                Niniejsza Polityka Prywatności i Polityka Cookies określa zasady przetwarzania i ochrony danych
-                osobowych przekazanych przez Użytkowników oraz plików Cookies, a także innych technologii pojawiających
-                się na stronie internetowej{' '}
+                {t(rodoContent.text1)}
                 <a className={styles.rodoLink} href="https://www.dynak.com" alt="dynak.com">
                     www.dynak.com
                 </a>
                 .
             </p>
+            <p>{t(rodoContent.text2)}</p>
+            <p>{t(rodoContent.text3)}</p>
+            <p>{t(rodoContent.definitions)}</p>
+            <span>{t(rodoContent.administrator)}</span>
+            <p>{t(rodoContent.user)}</p>
+            <span>{t(rodoContent.personalData)}</span>
             <p>
-                Administratorem strony i danych osobowych jest spółka dynak.com spółka z ograniczoną odpowiedzialnością
-                z siedzibą we Wrocławiu przy ul. Przyjaźni 36 d, (53-030), wpisaną przez Sąd Rejonowy dla
-                Wrocławia-Fabrycznej we Wrocławiu, VI wydział gospodarczy krajowego rejestru sądowego pod numerem:
-                0000777960, zwany dalej Administratorem.
-            </p>
-            <p>
-                Administrator zastrzega sobie prawo do wprowadzania zmian w polityce prywatności i polityce cookies, a
-                każdego Użytkownika strony obowiązuje znajomość aktualnej polityki prywatności. Przyczyną zmian mogą być
-                rozwój technologii internetowej, zmiany w powszechnie obowiązującym prawie czy też rozwój strony lub
-                bloga. Na dole strony znajduje się data publikacji aktualnej Polityki prywatności.
-            </p>
-            <p>DEFINICJE</p>
-            <span>2. „Administrator” – Spółka dynak.com spółka z ograniczoną odpowiedzialnością.</span>
-            <p>„Użytkownik” – każdy podmiot przebywający na stronie i korzystający z niej.</p>
-            <span>3. DANE OSOBOWE</span>
-            <p>
-                Dane osobowe przekazywane przez Użytkownika w ramach funkcjonowania Strony internetowej dynak.com,
-                profilu na{' '}
+                {t(rodoContent.paragraph1)}
                 <a
                     className={styles.rodoLink}
                     href="https://pl.linkedin.com/company/dynak-com?trk=public_profile_topcard_current_company"
                     alt="dynak.com na LinkedIn"
                 >
                     LinkedIn
-                </a>{' '}
-                mogą być przetwarzane w następujących celach i na następujących podstawach prawnych:
+                </a>
+                {t(rodoContent.paragraph2)}
             </p>
             <ol className="px-3">
+                <li>{t(rodoContent.paragraph3)}</li>
+                <li>{t(rodoContent.paragraph4)}</li>
+                <li>{t(rodoContent.paragraph5)}</li>
+                <li>{t(rodoContent.paragraph6)}</li>
+                <li>{t(rodoContent.paragraph7)}</li>
                 <li>
-                    wykonania usługi, przesłania oferty na prośbę Użytkownika — na podstawie art. 6 ust. 1 lit. b RODO
-                </li>
-                <li>wystawienia faktury — na podstawie art. 6 ust. 1 lit. c RODO;</li>
-                <li>
-                    kontaktu telefonicznego w sprawach realizacji usługi zgodnie z umową — na podstawie art. 6 ust. 1
-                    lit. b RODO;
-                </li>
-                <li>
-                    kontaktu telefonicznego w celu przedstawienia oferty i marketingu bezpośredniego – na podstawie art.
-                    6 ust. 1 lit. a RODO oraz na podstawie art. 6 ust. 1 lit. f RODO;
-                </li>
-                <li>
-                    tworzenia rejestrów związanych z RODO i innymi przepisami — na podstawie art. 6 ust. 1 lit. c RODO
-                    oraz art. 6 ust. 1 lit. f RODO;
-                </li>
-                <li>
-                    tworzenie analizy polegającej na analizie danych zbieranych automatycznie przy korzystaniu ze strony
-                    internetowej, w tym&nbsp;
+                    {t(rodoContent.paragraph8)}&nbsp;
                     <a className={styles.rodoLink} href="https://www.linkedin.com" alt="LinkedIn">
                         www.linkedin.com
                     </a>
@@ -82,39 +57,19 @@ const RodoContent = ({ rodoContent }) => {
                     ,{' '}
                     <a className={styles.rodoLink} href="https://www.gitlab.com" alt="GitLab">
                         www.gitlab.com
-                    </a>{' '}
-                    — na podstawie art. 6 ust. 1 lit. f RODO;
+                    </a>
+                    {t(rodoContent.paragraph9)}
                 </li>
-                <li>wykorzystywania cookies na Stronie i jej podstronach — na podstawie art. 6 ust. 1 lit. a RODO;</li>
-                <li>
-                    zarządzania Stroną internetową i stronami Administratora na innych platformach — na podstawie art. 6
-                    ust. 1 lit. f RODO (prawnie uzasadniony interes administratora);
-                </li>
-                <li>w celu tworzenia własnych baz danych Użytkowników – na podstawie art. 6 ust. 1 lit. f RODO;</li>
+                <li>{t(rodoContent.paragraph10)}</li>
+                <li>{t(rodoContent.paragraph11)}</li>
+                <li>{t(rodoContent.paragraph12)}</li>
             </ol>
+            <p>{t(rodoContent.paragraph13)}</p>
+            <p>{t(rodoContent.paragraph14)}</p>
+            <p>{t(rodoContent.paragraph15)}</p>
+            <p>{t(rodoContent.paragraph16)}</p>
             <p>
-                – Osoba, której dane dotyczą, ma prawo w dowolnym momencie wnieść sprzeciw – z przyczyn związanych z jej
-                szczególną sytuacją – wobec przetwarzania dotyczących jej danych osobowych opartego na art. 6 ust. 1
-                lit. e) lub f), w tym profilowania na podstawie tych przepisów. Administrator nie będzie, chyba że
-                wykaże on istnienie ważnych prawnie uzasadnionych podstaw do przetwarzania, nadrzędnych wobec interesów,
-                praw i wolności osoby, której dane dotyczą, lub podstaw do ustalenia, dochodzenia lub obrony roszczeń.
-            </p>
-            <p>
-                – Jeżeli osoba, której dane dotyczą, wniesie sprzeciw wobec przetwarzania do celów marketingu
-                bezpośredniego, danych osobowych nie będą już przetwarzane do takich celów.
-            </p>
-            <p>
-                – Najpóźniej przy okazji pierwszej komunikacji z osobą, której dane dotyczą, wyraźnie informuje się ją o
-                prawie, o którym mowa w ust. 1 i 2 RODO oraz przedstawia się je jasno i odrębnie od wszelkich innych
-                informacji.
-            </p>
-            <p>
-                – Jeżeli dane osobowe są przetwarzane do celów marketingu bezpośredniego, osoba, której dane dotyczą, ma
-                prawo wnieść w dowolnym momencie, bezpłatnie sprzeciw wobec tego przetwarzania, pierwotnego lub dalszego
-                – w tym profilowania, o ile jest ono powiązane z marketingiem bezpośrednim.
-            </p>
-            <p>
-                W tych celach może zwrócić się do Administratora poprzez adres e-mail: &nbsp;
+                {t(rodoContent.paragraph17)} &nbsp;
                 <a
                     className={styles.rodoLink}
                     href="mailto:admin@dynak.com?Subject=support"
@@ -125,53 +80,27 @@ const RodoContent = ({ rodoContent }) => {
                 </a>
                 .{' '}
             </p>
+            <p>{t(rodoContent.paragraph18)}</p>
+            <p>{t(rodoContent.paragraph19)}</p>
+            <p>{t(rodoContent.paragraph20)}</p>
+            <p>{t(rodoContent.paragraph21)}</p>
+            <p>{t(rodoContent.paragraph22)}</p>
+            <p>{t(rodoContent.paragraph23)}</p>
+            <span>{t(rodoContent.forms)}</span>
+            <p>{t(rodoContent.paragraph24)}</p>
+            <p>{t(rodoContent.paragraph25)}</p>
+            <span>{t(rodoContent.technologies)}</span>
             <p>
-                Zgoda Użytkownika może być w każdym czasie odwołana, cofnięta, co skutkować będzie usunięciem adresu
-                e-mail z listy mailingowej Administratora. Cofnięcie zgody pozostaje bez wpływu na przetwarzanie danych,
-                którego dokonano na podstawie zgody przed jej cofnięciem.
+                {t(rodoContent.Administrator)}
+                <b>{t(rodoContent.not)}</b> {t(rodoContent.paragraph26)}
             </p>
+            <span>{t(rodoContent.cookies)}</span>
             <p>
-                – Dane Użytkownika będą przechowywane przez Administratora przez czas realizacji poszczególnych usług.
+                {t(rodoContent.part1)}
+                <b>{t(rodoContent.not)}</b> {t(rodoContent.part2)} <b>{t(rodoContent.not)}</b> {t(rodoContent.part3)}{' '}
+                <b>{t(rodoContent.not)}</b> {t(rodoContent.part4)}
             </p>
-            <p>
-                – Użytkownik podaje dane dobrowolnie, jednakże jest to konieczne do osiągnięcia celu lub podjęcia
-                działań związanych z ich podaniem.
-            </p>
-            <p>
-                – Dane osobowe Użytkownika są przechowywane i chronione z należytą starannością, zgodnie ze wdrożonymi
-                procedurami wewnętrznymi Administratora. Administrator przetwarza informacje o Użytkowniku przy użyciu
-                odpowiednich środków technicznych i organizacyjnych spełniających wymagania przepisów prawa powszechnie
-                obowiązującego. Środki te mają na celu przede wszystkim zabezpieczenie danych osobowych Użytkowników
-                przed dostępem osób nieupoważnionych. W szczególności, dostęp do danych osobowych Użytkowników mają
-                jedynie osoby upoważnione, które są zobowiązane do zachowania tych danych w tajemnicy.
-            </p>
-            <p>
-                – Administrator Danych Osobowych niniejszym informuje, że nie powołał Inspektora Ochrony Danych
-                Osobowych (IODO) i wykonuje samodzielnie obowiązki związane z przetwarzaniem danych osobowych.
-            </p>
-            <p>
-                – Użytkownik przyjmuje do wiadomości, iż jego dane osobowe mogą być przekazywane uprawnionym organom
-                państwowym w związku z prowadzonymi przez nie postępowaniami, na ich żądanie i po spełnieniu przesłanek
-                potwierdzających niezbędność pozyskania od nas tych danych.
-            </p>
-            <span>4. FORMULARZE</span>
-            <p>Administrator stosuje następujące formularze w ramach Strony:</p>
-            <p>
-                – Formularz kontaktowy – umożliwia wysłanie wiadomości do spółki dynak.com spółka z ograniczoną
-                odpowiedzialnością. Dane przetwarzane są przez Administratora zgodnie z niniejszą Polityką Prywatności w
-                celu kontaktu z Użytkownikiem.
-            </p>
-            <span>5. TECHNOLOGIE</span>
-            <p>
-                Administrator <b>NIE</b> stosuje żadnych technologii obserwujących działań podejmowanych przez
-                Użytkownika w ramach Strony.
-            </p>
-            <span>6. PLIKI COOKIES</span>
-            <p>
-                Witryna dynak.com <b>NIE</b> używa plików cookies. Witryna <b>NIE</b> rozpoznaje rodzaju urządzenia, z
-                którego łączy się użytkownik. Parametry <b>NIE</b> pozwalają na odczytanie informacji w nich zawartych.
-            </p>
-            <span className="text-muted">Utworzono dnia 2020-03-16</span>
+            <span className="text-muted">{t(rodoContent.created)}</span>
         </div>
     );
 };
